695e4c0d395393408e5ea3ad3bacdc8ad7a56eaa
Menzo Windhouwer <menzo.windhouwer@di.huc.knaw.nl>
2025-07-13 21:38:54 +0200

    fixed paths

--- indexer/do-index.sh
+++ indexer/do-index.sh
@@ -7,14 +7,14 @@ uv lock
 source .venv/bin/activate
 
 echo "INSTITUTION"
-python read_and_index.py -d /Users/menzowi/Documents/GitHub/niod-dre-yugo-editor/data/apps/yugo/profiles/clarin.eu:cr1:p_1721373443934/records/ -t /Users/menzowi/Documents/Projects/yugo/indexer/configs/institution.toml --force
+python read_and_index.py -d /Users/menzowi/Documents/GitHub/niod-dre-yugo-editor/data/apps/yugo/profiles/clarin.eu:cr1:p_1721373443934/records/ -t /Users/menzowi/Documents/GitHub/niod-dre-yugo-browser/indexer/configs/institution.toml --force
 echo "COLLECTION"
-python read_and_index.py -d /Users/menzowi/Documents/GitHub/niod-dre-yugo-editor/data/apps/yugo/profiles/clarin.eu:cr1:p_1747312582429/records/ -t /Users/menzowi/Documents/Projects/yugo/indexer/configs/collection.toml --force
+python read_and_index.py -d /Users/menzowi/Documents/GitHub/niod-dre-yugo-editor/data/apps/yugo/profiles/clarin.eu:cr1:p_1747312582429/records/ -t /Users/menzowi/Documents/GitHub/niod-dre-yugo-browser/indexer/configs/collection.toml --force
 echo "PERSON"
-python read_and_index.py -d /Users/menzowi/Documents/GitHub/niod-dre-yugo-editor/data/apps/yugo/profiles/clarin.eu:cr1:p_1721373443955/records/ -t /Users/menzowi/Documents/Projects/yugo/indexer/configs/person.toml --force
+python read_and_index.py -d /Users/menzowi/Documents/GitHub/niod-dre-yugo-editor/data/apps/yugo/profiles/clarin.eu:cr1:p_1721373443955/records/ -t /Users/menzowi/Documents/GitHub/niod-dre-yugo-browser/indexer/configs/person.toml --force
 echo "GROUP"
-python read_and_index.py -d /Users/menzowi/Documents/GitHub/niod-dre-yugo-editor/data/apps/yugo/profiles/clarin.eu:cr1:p_1747312582450/records/ -t /Users/menzowi/Documents/Projects/yugo/indexer/configs/group.toml --force
+python read_and_index.py -d /Users/menzowi/Documents/GitHub/niod-dre-yugo-editor/data/apps/yugo/profiles/clarin.eu:cr1:p_1747312582450/records/ -t /Users/menzowi/Documents/GitHub/niod-dre-yugo-browser/indexer/configs/group.toml --force
 echo "EVENT"
-python read_and_index.py -d /Users/menzowi/Documents/GitHub/niod-dre-yugo-editor/data/apps/yugo/profiles/clarin.eu:cr1:p_1733830015132/records/ -t /Users/menzowi/Documents/Projects/yugo/indexer/configs/event.toml --force
+python read_and_index.py -d /Users/menzowi/Documents/GitHub/niod-dre-yugo-editor/data/apps/yugo/profiles/clarin.eu:cr1:p_1733830015132/records/ -t /Users/menzowi/Documents/GitHub/niod-dre-yugo-browser/indexer/configs/event.toml --force
 echo "PLACE"
-python read_and_index.py -d /Users/menzowi/Documents/GitHub/niod-dre-yugo-editor/data/apps/yugo/profiles/clarin.eu:cr1:p_1744616237113/records/ -t /Users/menzowi/Documents/Projects/yugo/indexer/configs/place.toml --force
+python read_and_index.py -d /Users/menzowi/Documents/GitHub/niod-dre-yugo-editor/data/apps/yugo/profiles/clarin.eu:cr1:p_1744616237113/records/ -t /Users/menzowi/Documents/GitHub/niod-dre-yugo-browser/indexer/configs/place.toml --force
