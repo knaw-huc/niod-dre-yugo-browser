89cb5a972e890e154c22ba8b5a2f168daa940c35
Menzo Windhouwer <menzo.windhouwer@di.huc.knaw.nl>
2025-07-22 17:39:41 +0200

    Update home.tsx

--- browser/frontend/src/components/home.tsx
+++ browser/frontend/src/components/home.tsx
@@ -12,8 +12,9 @@ export function Home() {
                 <p className="hcParagraph">
                     The Yugoslav Wars (1991-2001), one of the deadliest and most violent conflicts of the modern era, have been well documented in millions of objects, photos, videos, judicial records, letters, photos, videos, audio recordings, diaries, announcements, newspapers and much more. However, sources are scattered over dozens of organisations and communities and have not been made easily accessible for research purposes. It is not clear what is digitised, there are language barriers, metadata is missing and catalogues and finding aids exist only partly. These limitations severely restrict historical and legal research. It is difficult to enable new research avenues for a broader group of academics, media professionals, NGOs, victims, legal professionals and the interested public.
                 </p>
-                <img src={dayton}/>
             </div>
+            <br/>
+            <img src={dayton}/>
         </div>
     )
 }
